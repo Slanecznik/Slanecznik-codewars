92d6203e54febb808ccf2daa215317519e04e3a9
Slanecznik <vova.shigaev121183@gmail.com>
2025-09-09 22:18:34 +0200

    ReturnNegative

---
+++ 8kyu/ReturnNegative.js
@@ -0,0 +1,7 @@
+function makeNegative(num) {
+    return num > 0 ? -num : num
+}
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
